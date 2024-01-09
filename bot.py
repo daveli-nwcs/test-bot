271141832933c61170c1c779f79af817b2ecb1a0
daveli <daveli@k11.com>
2024-01-09 21:55:20 +0800

    update

--- bot.py
+++ bot.py
@@ -2,17 +2,24 @@ import os
 
 import telebot
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot("6963305401:AAHlMPzZ_bDVB0lO_6YpUoj6p9eQnJn3jQEso")
 
-@bot.message_handler(commands=['start', 'hello'])
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
+@bot.message_handler(commands=[‘info’])
+def send_info(message):
+    bot.reply_to(message, “This is a simple Telegram bot implemented in Python.”)
+
+# Define a message handler
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
 
-bot.infinity_polling()
+
+
+# Start the bot
+bot.polling()
 
