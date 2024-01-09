c66ff51355fac4ac6f4762c72d86f9112b5d9c7a
daveli <daveli@k11.com>
2024-01-09 23:24:25 +0800

    update: text

--- bot.py
+++ bot.py
@@ -44,33 +44,44 @@ def info_command(message):
 def callback(message):
     data = message.text
     if data.startswith('check-in-'):
-        check_in_queue(message.id , message.from_user.username)
+        check_in_queue(message)
     if data.startswith('check-out-'):
-        check_out_queue(message.id, message.from_user.username)
+        check_out_queue(message)
     if data.startswith('status'):
         print_queue(message)
     # bot.reply_to(message, message.text)
 
-def check_in_queue(id, username):
+def check_in_queue(message):
+    username = message.from_user.username
+    query_id = message.id
+    chat_id = message.chat.id
     if username not in current_users:
         current_users.append(username)
-        bot.answer_callback_query(id, text="You are checked-in successfully")
+        bot.answer_callback_query(query_id)
+        bot.send_message(chat_id=chat_id, text="You are checked-in successfully")
     else:
-        bot.answer_callback_query(id, text="You are already checked-in")
+        bot.answer_callback_query(query_id)
+        bot.send_message(chat_id=chat_id, text="You are already checked-in")
 
-def check_out_queue(id, username):
+def check_out_queue(message):
+    username = message.from_user.username
+    query_id = message.id
+    chat_id = message.chat.id
     if username in current_users:
         current_users.remove(username)
-        bot.answer_callback_query(id, text="You are checked-out successfully")
+        bot.answer_callback_query(id)
+        bot.send_message(chat_id=chat_id, text="You are checked-out successfully")
     else:
-        bot.answer_callback_query(id, text="You are not in the queue")
+        bot.answer_callback_query(id)
+        bot.send_message(chat_id=chat_id, text="You are not in the queue")
         
 def print_queue(message):
+    chat_id = message.chat.id
     result = "Current queue: "
     for index, item in enumerate(current_users):
         result += "\n" + str(index + 1) + ": " + item
     result += "\n Total: " + str(len(current_users))
-    bot.reply_to(message, result)
+    bot.send_message(chat_id, result)
 
 
 # Start the bot
